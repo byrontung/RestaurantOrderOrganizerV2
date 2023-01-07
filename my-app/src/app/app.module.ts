88b3606bb6c76799aef017e865d9b755e062cd13
byrontung <btung1@ualberta.ca>
2023-01-07 16:51:56 -0700

    creating foodview component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FoodViewComponent } from './food-view/food-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/food-view/food-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-view',
+  templateUrl: './food-view.component.html',
+  styleUrls: ['./food-view.component.css']
+})
+export class FoodViewComponent {
+
+}

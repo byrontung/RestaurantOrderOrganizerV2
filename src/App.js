9512a9579e6c7e4856efe524f0335e40904b487a
byrontung <btung1@ualberta.ca>
2023-01-07 19:45:34 -0700

    im bad wow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { FrontPage } from '../components/FrontPage/FrontPage';
+import { FrontPage } from './components/FrontPage/FrontPage';
 
 function App() {
   return (
